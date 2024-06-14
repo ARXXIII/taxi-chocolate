58c34f60a68fd587dba8fca67de119611e43be49
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-14 20:10:31 +0300

    Car card

---
+++ src/app/components/CarCard.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import { CarCardProps } from "../types"
+
+const CarCard = ({ url, brand, price, type, sale }: CarCardProps) => {
+    return (
+        <Link href={`/cars/${url}`} className="relative w-full py-12 text-center bg-dark-chocolate rounded-xl shadow">
+            {sale
+                ? (<div className="absolute top-3 left-3 px-2 text-center text-xl text-neutral-100 bg-red-500/80 rounded-lg shadow">Акция</div>)
+                : null
+            }
+            <section className="flex flex-col items-center gap-y-6 text-neutral-100">
+                <h1 className="font-bold text-3xl text-taxi shrink-0">{brand}</h1>
+                <h2 className="text-2xl shrink-0">Класс &#171;{type}&#187; АКПП</h2>
+                <p className="text-3xl shrink-0">Аренда от <strong className="text-taxi">{price}</strong> &#8381;</p>
+                <p className="text-2xl shrink-0">-50% + 8 выходных</p>
+            </section>
+        </Link>
+    )
+}
+
+export default CarCard
