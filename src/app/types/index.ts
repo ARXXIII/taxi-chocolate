0c6b3aa6d0a2042b5e52b35eaa32b5636945384a
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-14 20:10:59 +0300

    Types of Props

---
+++ src/app/types/index.ts
@@ -0,0 +1,12 @@
+export interface CarCardProps {
+	url: string;
+	brand: string;
+	type: string;
+	price: number;
+	text: string;
+	sale: boolean;
+}
+
+export interface CarPageProps {
+	params: any;
+}
