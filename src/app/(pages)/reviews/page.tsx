708c116c8bb30b2fc14453fce9d89aea87d14ecc
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-17 17:26:17 +0300

    Reviews

---
+++ src/app/(pages)/reviews/page.tsx
@@ -0,0 +1,27 @@
+import { reviews } from '@/app/constants'
+import { Feedback, PageHeading, ReviewForm } from '@/app/components'
+
+const Reviews = () => {
+    return (
+        <article className='flex flex-col items-center gap-y-6'>
+            <section className='lg:w-full'>
+                <PageHeading heading='Отзывы' />
+            </section>
+            {reviews
+                ? (
+                    <section className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
+                        {reviews.map((review, index) => (
+                            <Feedback key={index} name={review.name} review={review.review} />
+                        ))}
+                    </section>
+                )
+                : (null)}
+
+            <section className='w-full lg:w-fit'>
+                <ReviewForm />
+            </section>
+        </article>
+    )
+}
+
+export default Reviews
