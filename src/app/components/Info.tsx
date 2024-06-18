1e400200e1578445cc31346037a2eb43885e3709
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-18 17:47:49 +0300

    Info component

---
+++ src/app/components/Info.tsx
@@ -0,0 +1,44 @@
+import { InfoProps } from '../types'
+
+const Info = ({ title, list, subTitle, subList }: InfoProps) => {
+    return (
+        <div className='flex flex-col gap-y-3'>
+            <h1 className="p-3 text-center font-bold text-2xl lg:text-4xl text-taxi tracking-wide bg-dark-chocolate rounded-xl shadow">{title}</h1>
+            <div className="grid grid-cols-1 gap-y-3 text-xl">
+
+                {list?.map((info, index) => (
+                    <p key={index} className="leading-relaxed">{info}</p>
+                ))}
+
+                {subTitle
+                    ? (
+                        <div className="grid grid-cols-1 gap-y-3">
+                            <p>{subTitle}</p>
+                            <ul className="leading-relaxed">
+                                {subList?.map((item, index) => (
+                                    <li key={index}>
+                                        <span className='mr-3'>{index + 1}.</span>
+                                        {item}
+
+                                    </li>
+                                ))}
+                                <li>
+
+                                </li>
+                            </ul>
+                        </div>
+                    )
+                    : null
+                }
+
+            </div>
+            <div className='text-xl leading-relaxed'>
+                <p>
+                    Уточняйте подробности в офисе в отделе выкупа (2 этаж) или по телефону <a href="tel:+74959895333" className="p-1 text-taxi bg-dark-chocolate rounded-lg">+7&nbsp;(495)&nbsp;989-53-33</a> по будням с 9:00 до 18:00.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Info
