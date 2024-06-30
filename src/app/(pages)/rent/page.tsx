43cebf1732ba8157f24a1a9bee0a012b1d02d8c0
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-30 22:30:02 +0300

    Adapted cars grid

--- src/app/(pages)/rent/page.tsx
+++ src/app/(pages)/rent/page.tsx
@@ -28,7 +28,7 @@ const Rent = () => {
                 <div>
                     <List title="Требования для аренды" list={rentPageList} />
                 </div>
-                <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+                <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
 
                     {taxiPark.map((car) => (
                         <CarCard
